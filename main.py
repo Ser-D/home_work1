1357949f8bf6c160dae6a49cc639e4b0d156f80c
Ser-D <ipc.avanti@gmail.com>
2023-08-12 18:40:58 +0300

    Three

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-input()
-print("Hello")
 
+print("Hello")
+first_name = "Сергій"
+last_name = "Давидюк"
+full_name = first_name + last_name
+print(full_name)
+
+length = "2.75"
+width = "1.75"
+# length = int(length)
+# width = int(width)
+area = float(length) * float(width)
+show = f"With width {width} and length {length} of the room, its area is equal to {str(area)}"
+print(area)
