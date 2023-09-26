80c5c244cd5a2c39f621851b157de73113c53b64
Ser-D <ipc.avanti@gmail.com>
2023-09-26 13:11:48 +0300

    argv_test

--- main.py
+++ main.py
@@ -1,27 +1,19 @@
-
-
-result = None
-operand = None
-operator = None
-wait_for_number = True
-
-while True:
-    operand = input("ffre")
-    
-    try:
-        operand = int(operand)
-    except ValueError:
-        break
-    operator = input("rr")
-    if operator = "+":
-
-
+import os
 
 
     
+# def find_it(seq):
+#     for i in seq:
+#         print(i, seq.count(i) % 2)
+#         if seq.count(i) % 2 != 0:
+#             print(i, seq.count(i) % 2)
+#             return i
+            
+#     return None
 
+path = input('Введіть шлях до папки, що потребує сортування: ')
 
-    
-    
-
-
+if os.listdir(path):
+    print(os.listdir(path))
+else:
+    print("aaa")
