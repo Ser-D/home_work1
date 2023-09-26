d916d0a8a96f8766e57a1406740d5962a24582b0
Ser-D <ipc.avanti@gmail.com>
2023-09-26 12:44:51 +0300

    argv_test

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 
 
-message = input("Enter a message: ")
-offset = int(input("Enter the offset: "))
-encoded_message = ""
+result = None
+operand = None
+operator = None
+wait_for_number = True
 
-for ch in message:
-
-    if  ord(ch) >= 65 and ord(ch) <= 90:
-       ch_new = chr((ord(ch) - 65 + offset) % 26 + 65)
-    elif ord(ch) >= 97 and ord(ch) <= 122:
-        ch_new = chr((ord(ch) - 97 + offset) % 26 + 97)
-    else:
-         ch_new = ch
-    encoded_message += ch_new
+while True:
+    operand = input("ffre")
     
+    try:
+        operand = int(operand)
+    except ValueError:
+        break
+    operator = input("rr")
+    if operator = "+":
 
-print(encoded_message)
 
+
+
+    
 
 
     
